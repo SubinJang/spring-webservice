c4dfcdd1c2163eb75e412dc9a2842dc58c39bf0f
jsb <sbjang123456@gmail.com>
2019-07-23 17:04:09 +0900

    test code application.yml 추가

--- src/main/java/com/songjang/webservice/member/controller/MemberController.java
+++ src/main/java/com/songjang/webservice/member/controller/MemberController.java
@@ -3,10 +3,7 @@ package com.songjang.webservice.member.controller;
 import com.songjang.webservice.member.dto.MemberRequestDto;
 import com.songjang.webservice.member.dto.MemberResponseDto;
 import com.songjang.webservice.member.service.MemberService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -26,7 +23,7 @@ public class MemberController {
     }
 
     @GetMapping("/members")
-    public List<MemberResponseDto> findAll(){
-        return memberService.findAll();
+    public MemberRequestDto findAll(@ModelAttribute @Valid MemberRequestDto memberRequestDto){
+        return memberRequestDto;
     }
 }
